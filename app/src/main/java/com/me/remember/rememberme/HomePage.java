e0b51dc352ff2b9faf3bb65bcec0028d9de83232
Shoroq Suwan <shoroqsuwan@Shoroqs-MacBook-Pro.local>
2018-12-12 12:40:33 +0200

    Initial commit

---
+++ app/src/main/java/com/me/remember/rememberme/HomePage.java
@@ -0,0 +1,4 @@
+package com.me.remember.rememberme;
+
+public class HomePage {
+}
---
+++ app/src/main/java/com/me/remember/rememberme/MainActivity.java
@@ -0,0 +1,21 @@
+package com.me.remember.rememberme;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
